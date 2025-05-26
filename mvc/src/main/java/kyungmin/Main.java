d8252a5024470ddb1c277a362672ae812144cb06
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-26 13:36:46 +0900

    chore: src 제거

---
+++ mvc/src/main/java/kyungmin/Main.java
@@ -0,0 +1,11 @@
+package kyungmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+  public static void main(String[] args) {
+    SpringApplication.run(kyungmin.multimodule.MultiModuleApplication.class, args);
+  }
+}
