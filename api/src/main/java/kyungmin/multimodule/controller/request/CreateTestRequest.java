453ffb6de743324fd5e8aa1c9935dba0c5ef59e3
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-28 14:46:26 +0900

    add: Service 로직 추가

---
+++ api/src/main/java/kyungmin/multimodule/controller/request/CreateTestRequest.java
@@ -0,0 +1,6 @@
+package kyungmin.multimodule.controller.request;
+
+public record CreateTestRequest(
+  String title,
+  String content
+) { }
---
+++ api/src/main/java/kyungmin/multimodule/service/TestService.java
@@ -0,0 +1,26 @@
+package kyungmin.multimodule.service;
+
+import kyungmin.multimodule.controller.request.CreateTestRequest;
+import kyungmin.multimodule.domain.Test;
+import kyungmin.multimodule.repository.TestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+  private final TestRepository repository;
+
+  public void create(CreateTestRequest request) {
+    repository.save(
+      Test.builder()
+        .title(request.title())
+        .content(request.content())
+        .build()
+    );
+  }
+
+  public String getTitle(String id) {
+    return repository.findById(Long.parseLong(id)).get().getTitle();
+  }
+}
