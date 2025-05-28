b24e4a155f8f76d47f2ecbfed64315553403ddd7
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-28 14:50:31 +0900

    add: TestController 추가

---
+++ api/src/main/java/kyungmin/multimodule/controller/TestController.java
@@ -0,0 +1,25 @@
+package kyungmin.multimodule.controller;
+
+import kyungmin.multimodule.controller.request.CreateTestRequest;
+import kyungmin.multimodule.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/test")
+public class TestController {
+  private final TestService service;
+
+  @PostMapping(value = "/create")
+  public ResponseEntity<Void> create(@RequestBody CreateTestRequest request) {
+    service.create(request);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping(value = "/get")
+  public ResponseEntity<String> getTitle(String id) {
+    return ResponseEntity.ok(service.getTitle(id));
+  }
+}
